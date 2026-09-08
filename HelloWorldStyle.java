7bc406f7d45f27672302009bcaa05f4fd6a36955
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2026-09-08 13:18:13 +0000

    done

---
+++ HelloWorldStyle.java
@@ -0,0 +1,23 @@
+/**
+ * Hello World application demonstrating proper styling.
+ */
+public final class HelloWorldStyle {
+
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
+    private HelloWorldStyle() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    /**
+     * The main entry point of the application.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, Nebiyu!");
+        System.out.println("Ready for your final year?!");
+    }
+}
+
---
+++ package-info.java
@@ -0,0 +1,13 @@
+/**
+
+* This is the package-info for HelloWorldStyle.java.
+
+*
+
+* @author  Nebiyu Daniel
+
+* @version 1.0
+
+* @since   2026-09-08
+
+*/
